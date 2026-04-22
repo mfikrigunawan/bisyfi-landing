f5a3cf6cb01ee0704e6f94f7667335c17535f458
M Fikri Gunawan <muhammadfikrigunawan@gmail.com>
2026-04-22 15:55:51 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>🚀 Bisyfi</h1>
+      <h2>Jasa Pembuatan Website Modern</h2>
+      <p>
+        Kami membantu bisnis Anda memiliki website cepat, modern, dan
+        profesional.
+      </p>
+
+      <button>Hubungi Kami</button>
+    </div>
+  );
+}
+
+export default Home;
